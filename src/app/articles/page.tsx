60d75c591693fa17157bbdd2883ac0cf3df47e44
Navid <navidsotoudehmehr@gmail.com>
2023-07-26 13:01:30 +0330

    pagination added

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,20 +1,40 @@
 "use client";
+import { useState } from "react";
 import { useGetArticlesQuery } from "@/redux/services/article/articleApi";
 
 //component
 import ArticleCard from "../../components/pages/articles/ArticleCard";
+import SimplePagination from "../../components/common/pagination/SimplePagination";
+
 const Articles = () => {
+  const [pageNumber, setPageNumber] = useState();
+  console.log("pageNumber", pageNumber);
   const { data: articlesData, isFetching: articlesDataIsFetching } =
     useGetArticlesQuery(
-      { params: { pageNumber: 1, pageSize: 10 } },
+      { params: { page: pageNumber, pageSize: 3 } },
       { skip: !true, refetchOnMountOrArgChange: true }
     );
-
+  console.log("articlesData", articlesData);
   return (
     <div className="w-full py-4">
       {articlesData?.posts.map((articleData: any) => {
         return <ArticleCard articleData={articleData} key={articleData.id} />;
       })}
+      <div className="flex w-full justify-center py-[2px] ">
+        <div>
+          {articlesData && articlesData?.count && (
+            <SimplePagination
+              className="pagination-bar"
+              currentPage={pageNumber}
+              totalCount={articlesData?.count}
+              pageSize={3}
+              onPageChange={(e: number) => {
+                setPageNumber(e);
+              }}
+            />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/common/pagination/SimplePagination.tsx
@@ -0,0 +1,81 @@
+import { FaChevronRight } from "react-icons/fa";
+import { usePagination, DOTS } from "./usePagination";
+
+//styles are in /styles/pagination.css and imported in globals.css
+
+const Pagination = (props) => {
+  const {
+    onPageChange,
+    totalCount,
+    siblingCount = 1,
+    currentPage,
+    pageSize,
+  } = props;
+
+  const paginationRange = usePagination({
+    currentPage,
+    totalCount,
+    siblingCount,
+    pageSize,
+  });
+
+  if (currentPage === 0 || paginationRange?.length < 2) {
+    return null;
+  }
+
+  const onNext = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const onPrevious = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  const lastPage = paginationRange[paginationRange?.length - 1];
+  return (
+    <ul className={`pagination-container mt-5`}>
+      <li
+        className={` pagination-item
+      ${currentPage === 1 && "disabled"}`}
+        onClick={onPrevious}
+      >
+        <div className="arrow left">
+          <FaChevronRight />
+        </div>
+      </li>
+      {paginationRange.map((pageNumber, index) => {
+        if (pageNumber === DOTS) {
+          return (
+            <li key={index} className="pagination-item dots">
+              &#8230;
+            </li>
+          );
+        }
+
+        return (
+          <li
+            key={index}
+            className={`  pagination-item ${
+              pageNumber === currentPage && "selected"
+            }`}
+            onClick={() => onPageChange(pageNumber)}
+          >
+            {pageNumber}
+          </li>
+        );
+      })}
+      <li
+        className={`pagination-item ${currentPage === lastPage && "disabled"}`}
+        onClick={onNext}
+      >
+        <div className="arrow right">
+          <span className="rotate-180">
+            <FaChevronRight />
+          </span>
+        </div>
+      </li>
+    </ul>
+  );
+};
+
+export default Pagination;
